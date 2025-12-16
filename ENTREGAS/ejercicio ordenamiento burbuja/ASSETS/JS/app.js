d7760cd9da41fde7f693926f401aaed85e24612a
Patricio Vergara <p.vergara93@gmail.com>
2025-12-16 20:59:08 -0300

    se agrega entrega ordenamiento burbuja

--- ENTREGAS/ejercicio ordenamiento burbuja/ASSETS/JS/app.js
+++ ENTREGAS/ejercicio ordenamiento burbuja/ASSETS/JS/app.js
@@ -1,17 +1,27 @@
-window.alert("A continuación deberá ingresar 3 notas para calcular el promedio del alumno");
-var nota1=parseFloat(prompt("Ingrese Nota1:"));
-var nota2=parseFloat(prompt("Ingrese Nota2:"));
-var nota3=parseFloat(prompt("Ingrese Nota3:"));
+window.alert("Ingrese 3 números.");
 
-promedio=(nota1+nota2+nota3)/3;
+let lista=[]
+for (let i=0;i<3;i++){
+    contador=i+1;
+    var num = parseFloat(prompt("Ingrese número " + contador + ":"));
+    lista.push(num);
+}
 
-if (promedio>=6.0){
-    document.write("Excelente! Tienes promedio " + promedio);
-    }else if(promedio <6.0 && promedio>=5.0){
-        document("Tienes promedio " + promedio + ". Sigue adelante, puedes mejorar!");
-    }else if(promedio >=4.0 && promedio <5.0){
-        document.write("Tienes promedio " + promedio + ". Debes esforzarte más.");
-    }
-    else{
-        document.write("Has reprobado con promedio " + promedio + ". Continúa intentandolo");
-    }
+do{
+    n=0;
+    for (let i=1; i<lista.length; i++)
+      if (lista[i-1]>lista[i]){
+        temp=lista[i-1];
+        lista[i-1]=lista[i];
+        lista[i]=temp;
+        n=i;
+      } 
+} while (n!=0);
+
+if (lista[0]==lista[lista.length-1]){
+    document.write("Los 3 números son iguales");
+}
+else{
+    document.write("El menor de los números que ingresaste es "+ lista[0]);
+    document.write("<br/>El mayor de los números que ingresaste es "+ lista[lista.length-1]);
+}
